e054deb90a2a55aae9780d51a9f354f43cf038b7
apala-1 <apalalamichhane13579@gmail.com>
2025-12-23 02:38:39 +0545

    Navigation fixed and homepage creation started

---
+++ app/(public)/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+    return (
+        <div className="whole-page">
+            <div className="">
+                <div className="left">
+                    <img src="#" alt="logo" />
+                    <h2>Not A Writing App</h2>
+                </div>
+                <div className="right">
+                    <button>Login</button>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+            HomePage
+        </div>
+    );
+}
